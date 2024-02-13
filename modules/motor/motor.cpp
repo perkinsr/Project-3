76653b23eb532841f0d16af0d3a58654a9beee70
perkinsr <perkinsr@union.edu>
2024-02-13 21:40:28 +0000

    motor work

--- modules/motor/motor.cpp
+++ modules/motor/motor.cpp
@@ -1,39 +1,40 @@
 // Includes
 #include "mbed.h"
 #include "arm_book_lib.h"
+#include "motor.h"
 
 // Defines
 #define DUTY_MIN 0.023
 #define DUTY_MAX 0.112
 #define PERIOD 0.02
+#define INCREMENT 0.01
 
 // Objects
 PwmOut servo(PF_9);
 
 // Function prototypes
-int main();
 void servoInit();
 void servoUpdate();
-
-// Main
-int main() {
-    servoInit();
-    while (true) {
-    servoUpdate();
-    }
-}
 
 // Initialize the servo
 void servoInit() {
     servo.period(PERIOD); // 20ms period
-    servo.write(DUTY_MIN);
-    delay(2000);
 }
 
 // Set the movement of the servo
-void servoUpdate() {
+void servoUpdate( int placement) {
     servo.write(DUTY_MAX);
     delay(2000);
     servo.write(DUTY_MIN);
     delay(2000);
+}
+
+void servoSlow(){
+    servoInit();
+    float position = DUTY_MIN;
+    for (int i = 0; i < 100; i++){
+        position = position + INCREMENT;
+        servo.write(position);
+        delay(20);
+    }
 }
