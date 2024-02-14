6e6b2913759eadb677c7b281e6593918719dbbe8
BouvinGithub <bouving@union.edu>
2024-02-14 21:13:56 +0000

    start of wipersupdate

--- modules/motor/motor.cpp
+++ modules/motor/motor.cpp
@@ -3,7 +3,7 @@
 #include "arm_book_lib.h"
 #include "motor.h"
 // Defines
-
+#define SET_TIME_INTERVAL 40
 
 // Objects
 PwmOut servo(PF_9);
@@ -11,13 +11,15 @@ PwmOut servo(PF_9);
 // Function prototypes
 void servoInit();
 void servoUpdate();
+void servoSlow();
+float servoDegreesToPosition(float degrees);
 
 
 // Initialize the servo
 void servoInit() {
     servo.period(PERIOD); // 20ms period
     servo.write(DUTY_MIN);
-    delay(2000);
+    delay(1000);
 }
 
 // Set the movement of the servo
@@ -26,4 +28,35 @@ void servoUpdate() {
     delay(2000);
     servo.write(DUTY_MIN);
     delay(2000);
+}
+
+}
+
+void servoSlow(){
+    float position = DUTY_MIN;
+    for (int i = 0; i < 100; i++){
+        position = position + INCREMENT;
+        servo.write(position);
+        delay(50);
+    }
+}
+
+float servoDegreesToPosition(float degrees) {
+    if (degrees<180 && degrees >0) {
+        return degrees*(DUTY_MAX-DUTY_MIN)/180;
+    }
+    else {
+        return DUTY_MIN;
+    }
+}
+
+void servoWipersUpdate(int mode) {
+    static int accumulatedTime;
+    if (accumulatedTime>=SET_TIME_INTERVAL)
+        if (mode == OFF) {
+        }
+        if (mode == HIGH) {
+        }
+        if (mode == LOW) {
+        }
 }
